4d79104ee4e9fa9afc07d8c30c453241980caced
ilhamaditya101 <ilhamaditya1017@gmail.com>
2026-08-23 21:20:41 +0800

    Update CataclysmClassPlugin.java

--- src/main/java/com/cataclysm/classplugin/CataclysmClassPlugin.java
+++ src/main/java/com/cataclysm/classplugin/CataclysmClassPlugin.java
@@ -1,9 +1,70 @@
 package com.cataclysm.classplugin;
-import org.bukkit.plugin.java.JavaPlugin;
-import com.cataclysm.classplugin.manager.ClassManager;
+
+import com.cataclysm.classplugin.command.CCYCommand;
+import com.cataclysm.classplugin.command.ClassCommand;
 import com.cataclysm.classplugin.gui.ClassMenu;
-import com.cataclysm.classplugin.command.*;
-import com.cataclysm.classplugin.listener.XpListener;
+import com.cataclysm.classplugin.listener.ClassMenuListener;
+import com.cataclysm.classplugin.manager.ClassManager;
+
+import net.luckperms.api.LuckPerms;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class CataclysmClassPlugin extends JavaPlugin {
- public void onEnable(){saveDefaultConfig();saveResource("data.yml",false);ClassManager cm=new ClassManager(this);ClassMenu menu=new ClassMenu(cm);getCommand("class").setExecutor(new ClassCommand(menu,cm));getCommand("classadmin").setExecutor(new AdminCommand(this,cm));getServer().getPluginManager().registerEvents(menu,this);getServer().getPluginManager().registerEvents(new XpListener(cm),this);}
+
+    private LuckPerms luckPerms;
+    private ClassManager classManager;
+    private ClassMenu classMenu;
+
+    @Override
+    public void onEnable() {
+
+        saveDefaultConfig();
+
+        RegisteredServiceProvider<LuckPerms> provider =
+                Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+
+        if (provider == null) {
+            getLogger().severe("LuckPerms tidak ditemukan!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        luckPerms = provider.getProvider();
+
+        classManager = new ClassManager(this, luckPerms);
+        classMenu = new ClassMenu(this);
+
+        getCommand("class").setExecutor(
+                new ClassCommand(classMenu)
+        );
+
+        getCommand("ccy").setExecutor(
+                new CCYCommand(this)
+        );
+
+        Bukkit.getPluginManager().registerEvents(
+                new ClassMenuListener(
+                        this,
+                        classManager
+                ),
+                this
+        );
+
+        getLogger().info("CataclysmClass enabled!");
+    }
+
+    public ClassManager getClassManager() {
+        return classManager;
+    }
+
+    public ClassMenu getClassMenu() {
+        return classMenu;
+    }
+
+    public LuckPerms getLuckPerms() {
+        return luckPerms;
+    }
 }
